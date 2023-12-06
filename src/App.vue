891095514913144adce6e19270c0768943035ed6
danielecarpentiero <carpentierodaniele@gmail.com>
2023-12-06 18:00:38 +0100

    feat: scaffolding

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import AppTitle from "./components/AppTitle.vue";
+export default {
+  name: "App",
+  components: {
+    AppTitle,
+  },
+};
+</script>
+
+<template>
+  <AppTitle />
+</template>
+
+<style scoped></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>La mia prima app con Vite!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppTitle",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
